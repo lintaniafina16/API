ffb9d950a596ac9a65226ee2bc83a537884b4c52
lintaniafina16 <lintaniafina16@gmail.com>
2023-09-28 14:58:17 +0700

    tugas validate content

--- cypress/e2e/API/GetPoke.cy.js
+++ cypress/e2e/API/GetPoke.cy.js
@@ -4,3 +4,16 @@ it.only('Validate Header', () => {
     cy.get('@pokemon').its('headers').its('content-type')
         .should('include', 'application/json; charset=utf-8')
 });
+
+
+it.only('Validate Content', () => {
+    cy.request('https://pokeapi.co/api/v2/pokemon/bulbasaur').as('bulbasaur')
+    cy.get('@bulbasaur').its('status').should('equal', 200)
+    cy.get('@bulbasaur').its('body').should('include', {name: 'bulbasaur'})
+});
+
+it.only('Tugas Validate Content', () => {
+    cy.request({method: 'GET', url:'https://pokeapi.co/api/v2/ability/7/'}).then((response) =>{
+    expect(response.body.name).to.eq('limber')
+    })
+});
